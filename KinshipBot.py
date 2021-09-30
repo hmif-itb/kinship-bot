d006e2d2c46e0cb868ad49004bc589aaeb4f1b60
ravielze <ravielz20@gmail.com>
2021-09-30 18:24:35 +0700

    test

--- KinshipBot.py
+++ KinshipBot.py
@@ -57,17 +57,10 @@ class KinshipBot:
         if ('help' in msgSplit):
             payload = ("ReplyText", "Apa yang bisa dibantu?\nFoto: 'bot bagi foto nim xxx' atau 'bagi foto nim xxx dong bot'\n\nCek ultah: 'bot, siapa yang ultah hari ini?'\nSupported keyword: hari ini, besok, kemarin, lusa, lusa kemarin, x hari lagi, x hari lalu, minggu depan, minggu lalu\n\nCek ultah dalam range: 'bot, cek ultah x hari lagi'\nSupported Keyword: x hari lagi, x hari lalu\n\nSearch by nama panggilan: 'bot, bagi data dengan nama xxx' atau 'bagi data dengan nama xxx dong bot'\n\nKalau mau meme: 'bot, bagi meme dong' atau 'bagi meme ya bot'\n\nKalau mau dad jokes: 'bot, bagi jokes bapak2' atau 'bagi jokes bapak bapak dong bot'\n\nHal-hal umum seperti: 'hai bot', 'gws bot', 'thank you bot' juga akan gue balas\n\nKalau mau tanya siapa: 'bot siapa jodoh gue?' atau 'siapa jodoh kosar bot?'\n\nTanya pendapat ke bot: 'bot, apakah ...'\n\nButuh quote? 'bot, bagi quote dong'\n\nKalau mau this/that? 'bot pilih ... atau ... (atau ...)'\n\nMinta bot ngerating: 'bot, kasih rating buat ... dong'\n\nKalau mau generate random number: 'dari ... sampai ... berapa ...'\n\nDeskripsi random: 'bot, deskripsikan ...'\n\nMinta pendapat: 'bot, apa pendapat bot tentang ...?")
         elif ('testviel' in msgSplit):
-            payload = ("ReplyText", [{
-                'type': 'text',
-                'text': '@vl',
-                'mention': {
-                    'mentionees': {
-                        'index': 0,
-                        'length': 3,
-                        'userId': 'U5bfe9959a80d1783476fc79c97c298d8'
-                    }
-                }
-            }])
+            sourceType = event.source.type
+            if (sourceType == "group"):
+                sourceId = event.source.group_id
+                print(self.lineBotService.get_group_member_ids(sourceId))
         elif text_contains(msg, ['bagi', 'meme'], max_len=100):
             payload = self.memeService.reply()
             #foundNumber = False
